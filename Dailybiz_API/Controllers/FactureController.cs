e018cd9e14419c0d314a9323d2d4b31fcb88d5d7
Steven NICHOLLS <80457865+steven-wq@users.noreply.github.com>
2022-05-31 13:56:22 +0200

    Update FactureController.cs
    addition des routes

--- Dailybiz_API/Controllers/FactureController.cs
+++ Dailybiz_API/Controllers/FactureController.cs
@@ -12,6 +12,8 @@ namespace Dailybiz_API.Controllers
     public class FactureController : Controller
     {
         // GET v1/factures/{codeclient}
+        [HttpGet]
+        [Route("v1/factures/{codeclient}")]
         public string GetFacture(string CodeClient)
         {
             Facture facture = new Facture { };
@@ -59,6 +61,8 @@ namespace Dailybiz_API.Controllers
         // PUT v1/Facture
 
         // Ajouter un Facture
+        [HttpPut]
+        [Route("v1/Facture")]
         public string AddFacture(string cXml)
         {
             string cRetour = API.idev.InsererTable(cXml);
@@ -66,6 +70,8 @@ namespace Dailybiz_API.Controllers
         }
 
         // Supprimer un Facture
+        [HttpDelete]
+        [Route("v1/Facture/{id}")]
         public string DeleteFacture(string idFacture)
         {
             string cRetour = API.idev.SuppresionTable("FB_Factures", idFacture);
@@ -73,6 +79,8 @@ namespace Dailybiz_API.Controllers
         }
 
         // Mettre à jour un Facture
+        [HttpGet]
+        [Route("v1/Facture/{id}")]
         public string UpdateFacture(string cXml)
         {
             string cRetour = API.idev.MajTable(cXml);
