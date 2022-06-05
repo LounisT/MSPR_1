1d4e197399b073e4300d6a4feeb5e792dbbd6d37
Steven NICHOLLS <80457865+steven-wq@users.noreply.github.com>
2022-06-05 17:20:53 +0200

    Update API.cs

--- Dailybiz_API/Models/API.cs
+++ Dailybiz_API/Models/API.cs
@@ -1,24 +1,36 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
+
 using Dailybiz_API.com.dailybiz.exe;
 
 namespace Dailybiz_API.Models
 {
     public static class API
     {
-        public static Idylisapi idev = new com.dailybiz.exe.Idylisapi();
+        public static Idylisapi idev = new Idylisapi();
         public static string test;
         public static string cSessionID;
 
-        public static void setSession()
+
+
+        public static void setSession(/*string CodeAbonne = "mathiasapi", string Identifiant = "mathias.hue@hotmail.com", string MotDePasse = "Lenny27"*/)
         {
             String session = "";
-            session = API.idev.authentification1("mathiasapi", "mathias.hue@hotmail.com", "Lenny27");
-            API.idev.SessionIDHeaderValue = new com.dailybiz.exe.SessionIDHeader();
+            session = API.idev.authentification1(/*CodeAbonne*/ "mathiasapi", /*Identifiant*/ "mathias.hue@hotmail.com", /*MotDePasse*/"Lenny27");
+            API.idev.SessionIDHeaderValue = new SessionIDHeader();
             API.idev.SessionIDHeaderValue.SessionID = session;
-           
+
+            HttpContext context = HttpContext.Current;
+
+
+
+
         }
+
+     
+       
     }
 }
